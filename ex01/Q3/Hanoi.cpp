8b0347efa7219eb50aa31d357e68cc19b57c9207
Mai Kawauchi <49463452+MaiKawauchi@users.noreply.github.com>
2019-10-08 13:50:14 +0900

    Add files via upload

---
+++ ex01/Q3/Hanoi.cpp
@@ -0,0 +1,46 @@
+// Hanoi.cpp
+#include <iostream>
+#include <string>
+
+// COMPLETE:
+// Define the function Hanoi.
+// Its argument is the number of disks.
+//
+// The function should print a series of instructions for
+// how to move the disks in order to solve the problem.
+
+void Hanoi(int n, std::string from, std::string to, std::string work)
+{
+  	static unsigned int counter = 0;
+	if(n == 1){
+		std::cout << ++counter << ": " << from << " to " << to << std::endl;
+	}else{
+		Hanoi(n-1, from, work, to);
+		std::cout << ++counter << ": " << from << " to " << to << std::endl;
+		Hanoi(n-1, work, to, from);
+	}
+
+}
+
+
+
+
+int main(int argc, char** argv)
+{
+  // Input the circle count
+  if (argc!=2) {
+    std::cout << "Usage: " << argv[0] << " n" << std::endl;
+    std::cout << "where n is the number of disks." << std::endl;
+    return 1;
+  }
+
+  const std::string FROM = "A";
+  const std::string WORK = "B";
+  const std::string TO   = "C";
+
+  int n = std::stoi(argv[1]); // convert the argv[1] to an int
+
+  // COMPLETE:
+  // Call Hanoi with n disks
+  Hanoi(n, FROM, TO, WORK);
+}
