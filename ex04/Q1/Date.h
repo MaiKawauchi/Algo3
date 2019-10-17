dd7b6e2295f5b57c135666bad0e76c306282289f
Mai Kawauchi <49463452+MaiKawauchi@users.noreply.github.com>
2019-10-17 11:28:52 +0900

    Update Date.h

--- ex04/Q1/Date.h
+++ ex04/Q1/Date.h
@@ -4,11 +4,11 @@
 class Date {
 public:
   Date() {}
-  void set(int m=0, int d=0, int y=0);
-  void print();
-  const int get_month();
-  const int get_day();
-  const int get_year();
+  void set(const int m=0, const int d=0,const int y=0);
+  const void print();
+  inline const int get_month();
+  inline const int get_day();
+  inline const int get_year();
 private:
   int month, day, year;
 };
