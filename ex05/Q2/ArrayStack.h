cf861cf6b5dc0020d18257dfff5c18b7c5c19848
MaiKawauchi <s1250110@u-aizu.ac.jp>
2019-10-24 14:11:10 +0900

    fixed

--- ex05/Q2/ArrayStack.h
+++ ex05/Q2/ArrayStack.h
@@ -27,16 +27,23 @@ private:
   }
 
 public:
-  // Constructors:
+
+  // Default Constructors:
   ArrayStack()
   {
-<<<<<<< HEAD
     _num_items = 0;
     _allocated_size = 0;
     _items = nullptr;
   }
 
-  // Copy constructor
+  explicit ArrayStack(int allocated_size) : _items(new int[allocated_size])
+  {
+    _num_items = 0;
+    _items = new int[ allocated_size ];
+    _allocated_size = allocated_size;
+  }
+
+  /*---------------- Copy constructor ------------------*/
   ArrayStack (const ArrayStack& copy){
     std::cout << "Copy constructor!" << std::endl;
     _items = new int[copy._num_items];
@@ -47,13 +54,14 @@ public:
     }
   }
 
-  // Destructor
+
+  /*------------------- Destructor ---------------------*/
   virtual ~ArrayStack (){
     delete[] _items;
   }
 
-  // Copy assignment operator
-  ArrayStack& operator = (const ArrayStack& ope){
+  /*------------ Copy assignment operator --------------*/
+   ArrayStack& operator = (const ArrayStack& ope){
     std::cout << "Assignment operator!" << std::endl;
     delete[] _items;
     _items = new int[ope._num_items];
@@ -67,43 +75,9 @@ public:
 
 
 
-  explicit ArrayStack(int allocated_size) : _items(new int[allocated_size])
-  {
-    _num_items = 0;
-    _allocated_size = allocated_size;
-=======
-  _num_items = 0;
-  _allocated_size = 0;
-  _items = nullptr;
-  /*
-  ArrayStack(const ArrayStack& another_a)
-    {
-    _items = new int;
-    *_items = *another_a.a;}
-    */
-  }
-  explicit ArrayStack(int allocated_size) : _items(new int[allocated_size])
-  {
-    _num_items = 0;
-    // _allocated_size = 0;
-    //_items = new int;
-    //*_items = allocated_size;
-  }
-
-  // Destructor::
-  ~ArrayStack() {
-    // COMPLETE
-    delete[] _items;
->>>>>>> origin/master
-  }
-
   void push(int item) {
     if (_num_items == _allocated_size) resize(2 * _allocated_size);
     _items[_num_items++] = item;
-<<<<<<< HEAD
-=======
-    //    std::cout << item << std::endl;
->>>>>>> origin/master
   }
 
   int pop() {
@@ -116,10 +90,7 @@ public:
   bool empty() const { return _num_items == 0; }
 
   int size() const { return _num_items; }
-<<<<<<< HEAD
 }; // class ArrayStack};
-=======
-};
->>>>>>> origin/master
 
 #endif // ARRAY_STACK_H
+
