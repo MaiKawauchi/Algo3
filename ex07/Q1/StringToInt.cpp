aefa37f2fab675b1603b623b9b4a5b1d22af2afe
MaiKawauchi <s1250110@u-aizu.ac.jp>
2019-10-30 17:19:39 +0900

    fixed 10/30

--- ex07/Q1/StringToInt.cpp
+++ ex07/Q1/StringToInt.cpp
@@ -1,5 +1,4 @@
 // stringToInt.cpp
-
 #include <string>
 #include <sstream>
 #include <iostream>
@@ -14,40 +13,41 @@ int stringToInt(const string& input) {
 
   if (instream.fail()) {
     // Error: the input can not be converted
-    throw invalid_argument("input can not be converted to an int");
+    throw std::invalid_argument("input can not be converted to an int");
   }
 
   char left;
   instream >> left;
-  if (!instream.fail()){
+  if (!instream.fail()) {
     // Error: there are some characters left after the int
-      throw invalid_argument("input can not be converted to an int");
+    throw std::invalid_argument("input can not be converted to an int");
   }
 
   // everything went fine: returns the int
   return number;
- }
+}
+
 
 int main(void) {
   try{
-  string test1 = "11";
-  cout << stringToInt(test1) << endl;
-  }catch(const invalid_argument& error){
-    std::cout << error.what() << std::endl; 
+    string test1 = "11";
+    cout << stringToInt(test1) << endl;
+  }catch(const invalid_argument& error1){ // 不正な引数を示す
+    cout << error1.what() << endl;        //  what（）はエラーの内容を返す
   }
 
   try{
   string test2 = "cc11";
   cout << stringToInt(test2) << endl;
-  }catch(const invalid_argument& error){
-    std::cout << error.what() << std::endl; 
+  }catch(const invalid_argument& error2){
+    cout << error2.what() << endl;
   }
 
   try{
-  string test3 = "11cc";
-  cout << stringToInt(test3) << endl;
-  }catch(const invalid_argument& error){
-    std::cout << error.what() << std::endl; 
+    string test3 = "11cc";
+    cout << stringToInt(test3) << endl;
+  }catch(const invalid_argument& error3){
+    cout << error3.what() << endl;
   }
 
   cout << "Tests passed" << endl;
