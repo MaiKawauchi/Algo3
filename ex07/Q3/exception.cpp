bd34087defc0d45576e9644bbc795d911b21f023
MaiKawauchi <s1250110@u-aizu.ac.jp>
2019-11-06 15:09:37 +0900

    fixed 11/6

--- ex07/Q3/exception.cpp
+++ ex07/Q3/exception.cpp
@@ -6,34 +6,57 @@
 
 using namespace std;
 
+class File_ptr{
+  FILE* p;
+public:
+  File_ptr(const char* n, const char* a)
+    : p { fopen(n, a) }
+  {
+    if(p == nullptr)
+      throw std::runtime_error{ "File_ptr: Can't open file." };
+  }
+
+  File_ptr(const string& n, const char* a)
+    : File_ptr{ n.c_str(), a }
+  {}
+
+  explicit File_ptr(FILE* pp)
+    : p { pp }
+  {
+    if(p == nullptr)
+      throw std::runtime_error{ "File_ptr: nullptr." };
+  }
+
+  // destructor
+  ~File_ptr() { fclose(p); }
+
+  operator FILE*() { return p; }
+};
+
+
+
+
 void doSomeComputation() {
   throw std::runtime_error("failure during doing some computation");
 }
 
 void example() {
-  FILE* logfile = fopen("logfile.txt", "w+");
+  File_ptr logfile("logfile.txt", "w+");
 
   try{
-    if (!logfile) throw std::runtime_error("failed to open file");
+    if (!logfile) {
+      throw std::runtime_error("failed to open file");
+    }
     fputs("log1", logfile);
-  }catch(const std::runtime_error& error){
+  }
+  catch(const std::runtime_error& error){
     cout << error.what() << endl;
     fclose(logfile);
     throw;
   }
-
-
-  // call a function that performs some computation and may throw
-  // an exception
   try{
     doSomeComputation();
-  }
-   catch(const std::runtime_error& error){
-     cout << error.what() << endl;
-     fclose(logfile);
-     throw;
-    }
-
+  }catch(...){}
 
   /*------------------------------------------------*/
   cout << "closing logfile" << endl;
