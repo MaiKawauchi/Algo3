a05aae5171b22c5da014ac477f885b67c2cb04e8
MaiKawauchi <s1250110@u-aizu.ac.jp>
2019-11-11 19:05:27 +0900

    fixed 11/11

--- ex09/Q2/test_Mat_NxN.cpp
+++ ex09/Q2/test_Mat_NxN.cpp
@@ -5,17 +5,17 @@
 using namespace std;
 
 int main(void) {
-  int n;
-  cin >> n; // NxN行列
-
-  Mat_NxN<double, 4> A; // A = I
-  double coefficients[4][4] ={{1.0, 2.0, 3.0, 4.0}
+  //int n;
+  //  cin >> n; // NxN行列
+  const int n=5;
+  Mat_NxN<double, n> A; // A = I
+  double coefficients[n][n] ={{1.0, 2.0, 3.0, 4.0}
                               ,{5.0, 6.0, 7.0, 8.0}
                               ,{9.0, 10.0, 11.0, 12.0}
                               ,{13.0, 14.0, 15.0, 16.0}};
-  Mat_NxN<double, 4> B(coefficients);
+  Mat_NxN<double, n> B(coefficients);
 
-  Mat_NxN<double, 4> C = A * B; // OK
+  Mat_NxN<double, n> C = A * B; // OK
   assert(C == B);
 
 
@@ -26,7 +26,7 @@ int main(void) {
   // assert(C(1,1) == 9.0);
   //assert(C(2,2) == 17.0);
 
-  Mat_NxN<double, 4> D = -A; // OK
+  Mat_NxN<double, n> D = -A; // OK
 
   // assert(D(0,0) == -1.0);
   //assert(D(1,1) == -1.0);
