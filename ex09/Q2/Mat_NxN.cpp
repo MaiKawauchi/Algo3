fff9378b7c33ddc76638184ea101a9e595399946
MaiKawauchi <s1250110@u-aizu.ac.jp>
2019-11-11 18:45:04 +0900

    fixed 11/11

--- ex09/Q2/Mat_NxN.cpp
+++ ex09/Q2/Mat_NxN.cpp
@@ -11,105 +11,20 @@ Mat_NxN::Mat_NxN(const Mat_NxN& mat){
 }
 
 // defoult constructor
-Mat_NxN::Mat_NxN() {
-  for(int i=0; i<N; ++i){
-    for(int j=0; j<N; ++j){
-      dat[i][j] = 0.0;
-      if(i == j) dat[i][j] = 1.0;
-    }
-  }
-}
-
-Mat_NxN::Mat_NxN(const T coefficients[N][N]){
-  for(int i = 0; i < N; ++i){
-    for(int j = 0; j < N; ++j){
-      dat[i][j] = coefficients[i][j];
-    }
-  }
-}
 
 // destructor
-Mat_NxN::~Mat_NxN(){}
 
 /*------------------- 代入演算子 ------------------- */
-Mat_NxN& Mat_NxN::operator= (const Mat_NxN& mat) &{
-    for(int i = 0; i < N; ++i){
-      for(int j = 0; j < N; ++j){
-        dat[i][j] = mat.dat[i][j];
-      }
-    }
-    return *this;
-}
 
-Mat_NxN& Mat_NxN::operator= (Mat_NxN&& mat) & noexcept{
-  for(int i = 0; i < N; ++i){
-    for(int j = 0; j < N; ++j){
-      dat[i][j] = std::move(mat.dat[i][j]);
-    }
-  }
-  return *this;
-}
 
-/*--------------- 行列の各要素を参照 --------------- */
-T& Mat_NxN::operator() (int i, int j){
-  return dat[i][j];
-}
-
-T Mat_NxN::operator() (int i, int j) const{
-  return dat[i][j];
-}
 
 
 /*----------------- 複合代入演算子 ----------------- */
-/*-------------------- 複合加算 -------------------- */
 
-Mat_NxN& Mat_NxN::operator+= (const Mat_NxN &mat){
-for(int i=0; i<N; ++i){
-  for(int j=0; j<N; ++j){
-    dat[i][j] = dat[i][j] + mat.dat[i][j];
-  }
- }
- return *this;
-}
-
-/*-------------------- 複合減算 -------------------- */
-Mat_NxN& Mat_NxN::operator-= (const Mat_NxN &mat){
-for(int i=0; i<N; ++i){
-  for(int j=0; j<N; ++j){
-    dat[i][j] = dat[i][j] - mat.dat[i][j];
-  }
- }
- return *this;
-}
-
-/*-------------------- 複合乗算 -------------------- */
-Mat_NxN& Mat_NxN::operator*= (const Mat_NxN &mat){
-  Mat_NxN c = *this;
-for(int i=0; i<N; ++i){
-  for(int j=0; j<N; ++j){
-    dat[i][j] = 0.0;
-    for (int k=0; k<N; ++k) {
-      dat[i][j] += c.dat[i][k] * mat.dat[k][j];
-    }
-  }
- }
- return *this;
-}
 
 
 /*------------------- 単項マイナス ------------------- */
-// B = -A
-Mat_NxN Mat_NxN:::operator- () const{
-  Mat_NxN Nega;
-  for(int i=0; i<N; ++i){
-    for(int j=0; j<N; ++j){
-      if(dat[i][j] != 0){
-        Nega.dat[i][j] = -dat[i][j];
-      }
-    }
-  }
-  return Nega.dat;
-}
+
 
 
 
@@ -120,30 +35,12 @@ Mat_NxN Mat_NxN:::operator- () const{
 /*-------------------- 算術演算子 --------------------- */
 /*-------------------- 行列加算(+) -------------------- */
 
-Mat_NxN operator+ (const Mat_NxN& matA, const Mat_NxN& matB){
-  return Mat_NxN<T, N>(matA) += matB;
-}
 /*-------------------- 行列減算(-) -------------------- */
 
-Mat_NxN<T, N> operator- (const Mat_NxN<T, N>& matA, const Mat_NxN<T, N>& matB){
-  return Mat_NxN<T, N>(matA) -= matB;
-}
 /*-------------------- 行列乗算(x) -------------------- */
 
-Mat_NxN<T, N> operator* (const Mat_NxN<T, N>& matA, const Mat_NxN<T, N>& matB){
-  return Mat_NxN<T, N>(matA) *= matB;
-}
 
 
 
 /*------------------- 等価比較演算子 --------------------*/
 
-bool operator== (const Mat_NxN<T, N>& matA, const Mat_NxN<T, N>& matB) {
-  for(int i=0; i< N; ++i){
-    for(int j=0; j<N; ++j){
-
-      if( matA(i, j) != matB(i, j) ) return false;
-    }
-  }
-  return true;
-}
--- ex09/Q2/Mat_NxN.h
+++ ex09/Q2/Mat_NxN.h
@@ -14,40 +14,107 @@ template<typename T, int N>
 class Mat_NxN{
 public:
   // defoult constructor
-  Mat_NxN();
+  Mat_NxN(){
+    for(int i=0; i<N; ++i){
+      for(int j=0; j<N; ++j){
+        dat[i][j] = 0.0;
+        if(i == j) dat[i][j] = 1.0;
+      }
+    }
+  }
 
   // copy constructor(one argument)
   Mat_NxN(const Mat_NxN& mat);
 
-   // double 3x3 constructor
-  Mat_NxN(const double coefficients[N][N]);
+   // double NxN constructor
+  Mat_NxN(const T coefficients[N][N]){
+    for(int i = 0; i < N; ++i){
+      for(int j = 0; j < N; ++j){
+        dat[i][j] = coefficients[i][j];
+      }
+    }
+  }
 
   // destructor
-  ~Mat_NxN();
+  ~Mat_NxN(){}
 
-  Mat_NxN& operator= (const Mat_NxN& mat) &;   // 代入演算子
-  Mat_NxN& operator= (Mat_NxN&&) & noexcept;   // move代入演算子
+  /*------------------- 代入演算子 ------------------- */
+  Mat_NxN& operator= (const Mat_NxN& mat) &{
+    for(int i = 0; i < N; ++i){
+      for(int j = 0; j < N; ++j){
+        dat[i][j] = mat.dat[i][j];
+      }
+    }
+    return *this;
+  }
+   /*----------------- move代入演算子 ----------------- */
+  Mat_NxN& operator= (Mat_NxN&& mat) & noexcept{
+    for(int i = 0; i < N; ++i){
+      for(int j = 0; j < N; ++j){
+        dat[i][j] = std::move(mat.dat[i][j]);
+      }
+    }
+    return *this;
+  }
 
-  double& operator() (int i, int j);      // a(0, 0) == 1
-  double operator() (int i, int j) const; // a(0, 0) == 1
+  T& operator() (int i, int j) { return dat[i][j]; }      // a(0, 0) == 1
+  T operator() (int i, int j) const { return dat[i][j]; } // a(0, 0) == 1
 
 
   /*-------------------- 複合演算子 -------------------- */
-  // B = B + A
-  Mat_NxN& operator+= (const Mat_NxN& mat); // 行列の複合加算
-  Mat_NxN& operator-= (const Mat_NxN& mat); // 行列の複合減算
-  Mat_NxN& operator*= (const Mat_NxN& mat); // 行列の複合乗算
-  Mat_NxN& operator*= (double scale); // 行列の定数倍(Mat_NxN型の引数をとらない)
+  /*-------------------- 複合加算 -------------------- */
+  Mat_NxN& operator+= (const Mat_NxN& mat){
+    for(int i=0; i<N; ++i){
+      for(int j=0; j<N; ++j){
+        dat[i][j] = dat[i][j] + mat.dat[i][j];
+      }
+    }
+    return *this;
+  }
+  /*-------------------- 複合減算 -------------------- */
+  Mat_NxN& operator-= (const Mat_NxN& mat){
+    for(int i=0; i<N; ++i){
+      for(int j=0; j<N; ++j){
+        dat[i][j] = dat[i][j] - mat.dat[i][j];
+      }
+    }
+    return *this;
+  }
+  /*-------------------- 複合乗算 -------------------- */
+  Mat_NxN& operator*= (const Mat_NxN& mat){
+    Mat_NxN c = *this;
+    for(int i=0; i<N; ++i){
+      for(int j=0; j<N; ++j){
+        dat[i][j] = 0.0;
+        for (int k=0; k<N; ++k) {
+          dat[i][j] += c.dat[i][k] * mat.dat[k][j];
+        }
+      }
+    }
+    return *this;
+  }
+
+  // 行列の定数倍(Mat_NxN型の引数をとらない)
+  Mat_NxN& operator*= (T scale);
 
 
   /*------------------- 単項マイナス ------------------- */
   // B = -A
-  Mat_NxN operator- () const;
+  Mat_NxN operator- () const{
+    Mat_NxN Nega;
+    for(int i=0; i<N; ++i){
+      for(int j=0; j<N; ++j){
+        if(dat[i][j] != 0){
+          Nega.dat[i][j] = -dat[i][j];
+        }
+      }
+    }
+    return Nega.dat;
+  }
 
   /*------------- (i行、j列)要素にアクセス --------------*/
-  // double aij = A(i, j)
 private:
-  T dat[3][3];
+  T dat[N][N];
 };
 
 
@@ -55,29 +122,46 @@ private:
 
 /*------------------ グローバル関数 -------------------*/
 /*-------------------- 単項演算子 -------------------- */
-// C = A + B
+/*-------------------- 行列加算(+) -------------------- */
 template<typename T, int N>
-Mat_NxN<T, N> operator+ (const Mat_NxN<T, N>& matA, const Mat_NxN<T, N>& matB); // 行列の加算
+Mat_NxN<T, N> operator+ (const Mat_NxN<T, N>& matA, const Mat_NxN<T, N>& matB){
+  return Mat_NxN<T, N>(matA) += matB;
+}
+/*-------------------- 行列減算(-) -------------------- */
 template<typename T, int N>
-Mat_NxN<T, N> operator- (const Mat_NxN<T, N>& matA, const Mat_NxN<T, N>& matB); // 行列の減算
+Mat_NxN<T, N> operator- (const Mat_NxN<T, N>& matA, const Mat_NxN<T, N>& matB){
+  return Mat_NxN<T, N>(matA) -= matB;
+}
+/*-------------------- 行列乗算(x) -------------------- */
 template<typename T, int N>
-Mat_NxN<T, N> operator* (const Mat_NxN<T, N>& matA, const Mat_NxN<T, N>& matB); // 行列の乗算
+Mat_NxN<T, N> operator* (const Mat_NxN<T, N>& matA, const Mat_NxN<T, N>& matB){
+  return Mat_NxN<T, N>(matA) *= matB;
+}
+
+
 
 /*---------------- ストリーム挿入演算子 ----------------*/
 // C++ストリームにMat_NxNオブジェクトを挿入
-/*
-template<class Char, class Traits>
-std::basic_ostream<Char, Traits>& operator<<
-(std::basic_ostream<char, Traits>& os, const Mat_NxN&);
 
-template<class Char, class Traits>
+template<class Char, class Traits, typename T, int N>
+std::basic_ostream<Char, Traits>& operator<<
+(std::basic_ostream<char, Traits>& os, const Mat_NxN<T, N>& mat);
+
+template<class Char, class Traits, typename T, int N>
 std::basic_istream<Char, Traits>& operator>>
-(std::basic_istream<char, Traits>& is, const Mat_NxN&);
-*/
+(std::basic_istream<char, Traits>& is, const Mat_NxN<T, N>& mat);
+
 /*-------------------- 等価比較演算子 ------------------- */
 // if(A == B)
 template<typename T, int N>
-bool operator== (const Mat_NxN<T, N>& matA, const Mat_NxN<T, N>& matB);
+bool operator== (const Mat_NxN<T, N>& matA, const Mat_NxN<T, N>& matB){
+  for(int i=0; i< N; ++i){
+    for(int j=0; j<N; ++j){
+      if( matA(i, j) != matB(i, j) ) return false;
+    }
+  }
+  return true;
+}
 
 
 #endif
